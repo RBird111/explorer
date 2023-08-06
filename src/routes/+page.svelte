64037f5a3c9e29040f328074c466dd8347daa279
Roosevelt Burden <rooseveltburden@gmail.com>
2023-08-05 22:42:57 -0600

    custom title bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import { type Directory, typewriter } from '$lib';
 
+	import { appWindow } from '@tauri-apps/api/window';
 	import { invoke } from '@tauri-apps/api/tauri';
 	import { onMount } from 'svelte';
 	import File from '$lib/components/File.svelte';
+	import MinIcon from '$lib/assets/MinIcon.svelte';
+	import MaxIcon from '$lib/assets/MaxIcon.svelte';
+	import CloseIcon from '$lib/assets/CloseIcon.svelte';
 
 	let directory: Directory | null = null;
 
@@ -19,11 +23,42 @@
 
 <div class="wrap">
 	{#if directory}
-		{#key directory.dir}
-			<div class="directory" in:typewriter={{ speed: 6 }}>
-				{directory.dir}
+		<div data-tauri-drag-region class="title-bar">
+			{#key directory.dir}
+				<div class="directory" in:typewriter={{ speed: 6 }}>
+					{directory.dir}
+				</div>
+			{/key}
+
+			<div class="title-buttons">
+				<button
+					class="title-button"
+					on:click={() => {
+						appWindow.minimize();
+					}}
+				>
+					<MinIcon />
+				</button>
+
+				<button
+					class="title-button"
+					on:click={() => {
+						appWindow.maximize();
+					}}
+				>
+					<MaxIcon />
+				</button>
+
+				<button
+					class="title-button"
+					on:click={() => {
+						appWindow.close();
+					}}
+				>
+					<CloseIcon />
+				</button>
 			</div>
-		{/key}
+		</div>
 
 		<div class="files">
 			<button class="file-icon" value="What" on:click={goUp}>
@@ -47,18 +82,27 @@
 		display: grid;
 		grid-template-rows: 1fr 19fr;
 
-		.directory {
+		.title-bar {
 			@include txt-code;
 
 			display: flex;
-			justify-content: flex-start;
+			justify-content: space-between;
 			align-items: center;
-
 			font-weight: 400;
-
 			background-color: $c-main;
 			color: $c-background;
-			padding: 0 0.3rem;
+			padding: 0 5px;
+			height: 30px;
+
+			.title-buttons {
+				display: flex;
+
+				.title-button {
+					display: flex;
+					justify-content: center;
+					align-items: center;
+				}
+			}
 		}
 
 		.files {
